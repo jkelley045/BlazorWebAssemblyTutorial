ed93781b0241a1286d71f25ee25bd07607f4fda7
Jake Kelley <jkelley@instant-tech.com>
2021-08-29 16:01:24 -0400

    Configuring Services to include the AddDbContext class

--- BlazorProject/Server/Startup.cs
+++ BlazorProject/Server/Startup.cs
@@ -1,7 +1,9 @@
+using BlazorProject.Server.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,6 +24,9 @@ namespace BlazorProject.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DBConnection")));
+
 
             services.AddControllersWithViews();
             services.AddRazorPages();
